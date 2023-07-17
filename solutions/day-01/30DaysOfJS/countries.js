7325c72c2e18d18aa5457c2fb908432960bf3e55
preston176 <prestonnyamweya@gmail.com>
2023-07-17 21:08:06 +0300

    lv2 done

--- solutions/day-01/30DaysOfJS/countries.js
+++ solutions/day-01/30DaysOfJS/countries.js
@@ -1,4 +1,23 @@
 let countries =['Kenya', 'Tanzania', 'India', 'Canada']
+console.log(countries);
 
+// if (countries.values("Kenya"))
+{
+    // console.log(true);
+    
+}
+// es6 ternary
 
+const checkEthiopia = () => {
+    countries.includes("Ethiopia") ? console.log(true): console.log("false");
+}
+checkEthiopia()
 // module.exports = countries;
+
+
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+const backEnd = ['Node', 'Express', 'MongoDB']
+
+const fullStack = frontEnd.concat(backEnd)
+
+console.log(fullStack)
