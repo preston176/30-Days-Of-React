be1faba1f3de9d94304d4e2bc6e0d215f2b4988a
preston176 <prestonnyamweya@gmail.com>
2023-07-18 02:58:15 +0300

    areaofcircle done

--- solutions/day-01/30DaysOfJS/Functions/Functions.js
+++ solutions/day-01/30DaysOfJS/Functions/Functions.js
@@ -7,10 +7,19 @@
 // fullName("preston", "mayieka")
 
 
-const addNumbers = (x,y) => {
-    console.log(x + y);
+// const addNumbers = (x,y) => {
+//     console.log(x + y);
     
-    return x + y
-}
+//     return x + y
+// }
 
-addNumbers(3,4)
+// addNumbers(3,4)
+
+
+// const areaOfCirle = (r) => {
+//     const pi = 3.14
+//     console.log((pi * r * r).toFixed(2));
+//     return (pi * r * r).toFixed(2)
+// }
+
+// areaOfCirle(9)
