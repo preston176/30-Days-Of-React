ec3b9012ee374cac2592853363359f664ecfaffb
preston176 <prestonnyamweya@gmail.com>
2023-07-18 03:12:03 +0300

    BMI done

--- solutions/day-01/30DaysOfJS/Functions/Functions.js
+++ solutions/day-01/30DaysOfJS/Functions/Functions.js
@@ -35,11 +35,35 @@
 
 // convertCelciusToFahrenheit(20)
 
-// const calcBmi = (weight,height) => {
-//     let bmi = (weight / (height * height))
-//     console.log(bmi);
-//     return bmi;
+const calcBmi = (weight,height) => {
+    let bmi = (weight / (height * height))
+    console.log("your bmi is:",bmi.toFixed(1));
+    return bmi.toFixed(1);
     
-// }
+}
 
-// calcBmi(30,5.5)
+const checkWeight = (e) => {
+    let status;
+    if (e < 18.5)
+    {
+        console.log("Underweight");
+        status = "Underweight"
+    }
+    if (e > 18.5 && e < 24.9)
+    {
+        console.log("Normal Weight");
+        status = "Normal Weight"
+    }
+    if (e > 25 && e < 29.9)
+    {
+        console.log("Overweight");
+        status = "Overweight"
+    }
+    if (e >=30)
+    {
+        console.log("Obese");
+        status = "Obese"
+    }
+}
+
+checkWeight(calcBmi(50,1.8))
