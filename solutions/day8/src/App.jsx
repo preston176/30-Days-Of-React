d92fc8690026881e2b214f2aa92754c6a17e479f
preston176 <prestonnyamweya@gmail.com>
2023-07-19 21:20:20 +0300

    darkmode

--- solutions/day8/src/App.jsx
+++ solutions/day8/src/App.jsx
@@ -1,13 +1,29 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
+const [darkMode, setDarkmode] = useState(true)
+
+function onClicked () {
+  setDarkmode((prev) => !prev)
+  console.log("dark mode enabled:",darkMode);
+
+}
 
   return (
-    <>
-      
-    </>
+    <div
+      style={{
+        height: "100vh",
+        width: "100%",
+        backgroundColor: darkMode ? "black" : "lightgray",
+      }}
+    >
+
+    <button onClick={onClicked} style={{transform: "scale(1.1)", margin: "auto"}}>Dark Mode 😒</button>
+
+    </div>
   )
 }
 
