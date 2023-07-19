aafd223f0166dc2510a229317d8b88471853ca94
preston176 <prestonnyamweya@gmail.com>
2023-07-19 21:27:48 +0300

    state as props to child

--- solutions/day8/src/App.jsx
+++ solutions/day8/src/App.jsx
@@ -1,6 +1,7 @@
 
 import { useState } from 'react'
 import './App.css'
+import Cards from './Components/Cards';
 
 function App() {
 
@@ -9,7 +10,6 @@ const [darkMode, setDarkmode] = useState(true)
 function onClicked () {
   setDarkmode((prev) => !prev)
   console.log("dark mode enabled:",darkMode);
-
 }
 
   return (
@@ -20,9 +20,9 @@ function onClicked () {
         backgroundColor: darkMode ? "black" : "lightgray",
       }}
     >
-
+    
     <button onClick={onClicked} style={{transform: "scale(1.1)", margin: "auto"}}>Dark Mode 😒</button>
-
+    <Cards whichMode={darkMode} />
     </div>
   )
 }
---
+++ solutions/day8/src/Components/Cards.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Cards({whichMode}) {
+
+
+
+
+  return (
+    <div className='cards-container'>
+        <div className="cards-wrapper" style={{backgroundColor: whichMode ? "red" : "green"}}>
+            <div className="card">
+                Card
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Cards
