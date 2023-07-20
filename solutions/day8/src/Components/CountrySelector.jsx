e88ea8252b53fa2267d8ac4562e0968e9d54437a
preston176 <prestonnyamweya@gmail.com>
2023-07-20 14:42:46 +0300

    country name 0

--- solutions/day8/src/Components/CountrySelector.jsx
+++ solutions/day8/src/Components/CountrySelector.jsx
@@ -1,20 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+const countriesArray = [{
+    image: "",
+    Name: "Kenya",
+    Capital: "Nairobi",
+    Languages: "English, Kiswahili",
+    Population: "900 0000",
+    Currency : "KES"
+},{},{},{}]
+
+
+
+console.log(countriesArray[0]);
+
+
 
 function CountrySelector() {
+
+const [countryState ,SetCountryState] = useState(0);
+
+const displayCountry = countriesArray[countryState]
+
+
+console.log(displayCountry.Name);
+    
   return (
     <div>
 
     <div className="country-img-name">
     <img src="" alt="country-img" />
-    <p>Country Name</p>
+    <p>Country Name : {displayCountry.Name}</p>
     </div>
 
     <div className="country-stats">
 
-        <p>Capital: </p>
-        <p>Languages: </p>
-        <p>Population: </p>
-        <p>Currency: </p>
+        <p>Capital: {displayCountry.Capital}</p>
+        <p>Languages:{displayCountry.Languages} </p>
+        <p>Population: {displayCountry.Population}</p>
+        <p>Currency: {displayCountry.Currency}</p>
     </div>
 
 
