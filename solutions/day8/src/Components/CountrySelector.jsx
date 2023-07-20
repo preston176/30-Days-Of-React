07b950ddf017518923f2f98fdb9e41079ab84b69
preston176 <prestonnyamweya@gmail.com>
2023-07-20 14:44:25 +0300

    nigeria added

--- solutions/day8/src/Components/CountrySelector.jsx
+++ solutions/day8/src/Components/CountrySelector.jsx
@@ -7,7 +7,14 @@ const countriesArray = [{
     Languages: "English, Kiswahili",
     Population: "900 0000",
     Currency : "KES"
-},{},{},{}]
+},{
+    image: "",
+    Name: "Nigeria",
+    Capital:"Lagos",
+    Languages: "English, Kiswahili",
+    Population: "100,2944,000",
+    Currency : "NAIRA" 
+},{},{}]
 
 
 
@@ -28,7 +35,7 @@ console.log(displayCountry.Name);
     <div>
 
     <div className="country-img-name">
-    <img src="" alt="country-img" />
+    <img src={displayCountry.image} alt="country-img" />
     <p>Country Name : {displayCountry.Name}</p>
     </div>
 
