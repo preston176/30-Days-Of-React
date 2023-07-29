602e5fb2dd67abfd9cb0203d10208d5864d3c437
preston176 <prestonnyamweya@gmail.com>
2023-07-29 15:33:44 +0300

    clean

--- solutions/day19/src/App.jsx
+++ solutions/day19/src/App.jsx
@@ -1,70 +1,32 @@
-
-import { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [totalCats, setTotalCats] = useState([])
-  const [avgAges, setAvgAges] = useState()
-  const [avgWeigh, setAvgWeigh] = useState()
-  useEffect(() => {
-    fetch('https://api.thecatapi.com/v1/breeds')
-      .then(response => response.json())
-      .then(data => {
-        const averageAges = [];
-        data.forEach(cat => {
-          let life = cat.life_span;
-          // console.log(life);
+const App = () => {
+  const [card, setCard] = useState([]);
+  const [selections, setSelections] = useState([])
 
-          let ages = life.split('-').map(Number);
-          const sum = ages.reduce((a, b) => a + b, 0);
-          let avgAge = sum / ages.length;
-          // console.log(avgAge);
-  
-          averageAges.push(avgAge);
-        });
+  const handleclick = (e) => {
 
-        const overallAvgAge = averageAges.reduce((a, b) => a + b, 0) / averageAges.length;
-        console.log('Overall Average Age:', overallAvgAge);
-
-        setAvgAges(overallAvgAge.toFixed(1))
-        setTotalCats(data.length);
-      
-        const averageWeights = [];
-
-        data.forEach(cat => {
-          let weight = cat.weight.metric;
-          console.log(weight);
-  
-          let weights = weight.split('-').map(Number);
-          const sum = weights.reduce((a, b) => a + b, 0);
-          let avgWeights = sum / weights.length;
-          console.log('Average Weight for Cat:', avgWeights);
-  
-          // Store the average weight for each cat in the array
-          averageWeights.push(avgWeights);
-        });
-  
-        // Calculate the overall average weight for all cats
-        const overallAvgWeight = averageWeights.reduce((a, b) => a + b, 0) / averageWeights.length;
-        console.log('Overall Average Weight:', overallAvgWeight)
-      
-        setAvgWeigh(overallAvgWeight.toFixed(1))
-      });
-
-  }, []);
-  
-
+ setCard([...card, e.target.textContent])
+   
+    // console.log(e.target.textContent)
+    
+  }
 
   return (
-    <div className='form-control justify-content-center text-center'>
-      <h1>30 Days of react</h1>
-      <div className="cats">
-        <h3>Cats paradise</h3>
-        <div className="stats">
-          <p>Total cats: {totalCats}</p>
-          <p>Average Age : {avgAges}</p>
-          <p>Average weight: {avgWeigh}</p>
-        </div>
+    <div className='container1'>
+      <div className="card__container">
+        {/* {card.map((item, index) => {
+          return (<div key={index} className="card select">{item}</div>)
+        })}  */}
+        {card.map((item, index) => (
+          <div className="card select" key={index}>{item}</div>
+        ))}
+      </div>
+      <div className="selctions">
+        <div onClick={handleclick} className="card select">card 1</div>
+        <div onClick={handleclick} className="card select">card 2</div>
+        <div onClick={handleclick} className="card select">card 3</div>
       </div>
     </div>
   )
